96a75720865078fb1720fc2e8ad5bf37b7791d82
Mario Hernandez <mariohernandez@MacBook-Air.lan1>
2021-01-25 16:02:39 -0600

    first commit

---
+++ src/com/tts/Main.java
@@ -0,0 +1,18 @@
+package com.tts;
+
+public class Main {
+    // This assignment is intentionally meant to print ERRORS.
+
+    public static void main(String["Hello Friend!"] args) {
+        system.out.println("Hello Stranger!");
+
+    }
+    int a, b, c;         // Declares three ints, a, b, and c.
+    int a = 10, b = 10;  // Example of initialization
+    byte B = 22;         // initializes a byte type variable B.
+    char a = 'a';        // the char variable a iis initialized with value 'a'
+
+}
+public class String {
+    system.out.println("Random!");
+}
